0e45be441550bec22293e20fccc24439af92cfde
Quty <quty.jain@gmail.com>
2018-05-17 15:33:21 -0400

    updating links for services

--- src/main/java/com/books/zipkinSvr/ZipkinSvrApplication.java
+++ src/main/java/com/books/zipkinSvr/ZipkinSvrApplication.java
@@ -2,9 +2,6 @@ package com.books.zipkinSvr;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.sleuth.Sampler;
-import org.springframework.cloud.sleuth.sampler.AlwaysSampler;
-import org.springframework.context.annotation.Bean;
 
 import zipkin.server.EnableZipkinServer;
 
